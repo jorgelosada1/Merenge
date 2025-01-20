a25e587da377e25221253a58139ba4a3687634ac
Jorge Losada <jorgeandresvargas123@gmail.com>
2025-01-19 21:15:49 -0500

    Actualizar productos.json y mejorar la carga en el componente Catalog

--- src/components/Catalog.jsx
+++ src/components/Catalog.jsx
@@ -1,11 +1,7 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Typography, Grid, ThemeProvider, createTheme } from '@mui/material';
 import RecipeReviewCard from './Card.jsx';
 
-// Cargar el archivo JSON
-import productos from './productos.json';
-
-// Crear el tema con la fuente "Pacifico"
 const theme = createTheme({
   typography: {
     fontFamily: 'Pacifico, cursive',
@@ -15,13 +11,15 @@ const theme = createTheme({
 const Catalog = () => {
   const [products, setProducts] = useState([]);
 
-  // Usamos useEffect para cargar los datos del archivo JSON al montar el componente
   useEffect(() => {
-    setProducts(productos.merengones);  // Cargar los productos del archivo JSON
+    fetch('public/Productos.json')  // La ruta es relativa a la carpeta 'public'
+      .then(response => response.json())
+      .then(data => setProducts(data.merengones))
+      .catch(error => console.error('Error al cargar los productos:', error));
   }, []);
 
   return (
-    <ThemeProvider theme={theme}> {/* Aplicamos el tema */}
+    <ThemeProvider theme={theme}>
       <div id="catalog" style={{ padding: '20px', textAlign: 'center', backgroundColor: '#e1bee7' }}>
         <Typography
           variant="h4"
@@ -33,21 +31,20 @@ const Catalog = () => {
           Catálogo de Productos
         </Typography>
         
-        {/* Usamos Grid para un diseño responsivo */}
         <Grid container spacing={3} justifyContent="center" style={{ marginTop: '20px' }}>
           {products.map((product, index) => (
-            <Grid item key={index} xs={12} sm={6} md={4} lg={4}> {/* Cambié lg={4} para 3 productos por fila */}
+            <Grid item key={index} xs={12} sm={6} md={4} lg={4}>
               <RecipeReviewCard 
                 nombre={product.nombre} 
                 descripcion={product.descripcion} 
                 imagen={product.imagen} 
-                descripcionStyle={{ fontFamily: 'Arial, sans-serif' }} // Aquí cambiamos solo la fuente de la descripción
+                descripcionStyle={{ fontFamily: 'Arial, sans-serif' }}
               />
             </Grid>
           ))}
         </Grid>
       </div>
-    </ThemeProvider> 
+    </ThemeProvider>
   );
 };
 
